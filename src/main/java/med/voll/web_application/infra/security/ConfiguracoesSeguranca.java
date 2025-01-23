b4998bd0d438a619a0f9347148281369f8a42ca1
tcancissu <thiago.cancissu@gmail.com>
2025-01-23 06:15:32 -0300

    Gerenciandoo sessão com filtro rememberme

--- src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
+++ src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
@@ -2,6 +2,7 @@ package med.voll.web_application.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -13,7 +14,6 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class ConfiguracoesSeguranca {
-
     @Bean
     public UserDetailsService dadosUsuariosCadastrados(){
         UserDetails usuario1 = User.builder()
@@ -40,6 +40,10 @@ public class ConfiguracoesSeguranca {
                 .logout(logout -> logout
                         .logoutSuccessUrl("/login?logout")
                         .permitAll())
+                .rememberMe(rememberMe -> rememberMe.key("lembrarDeMim")
+                        .alwaysRemember(true)
+                )
+                .csrf(Customizer.withDefaults())
                 .build();
     }
 }
