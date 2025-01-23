8b3e204139c9c73c2d051d51e4c829de943e3cbc
Thiago Cancissú <thiago.cancissu@gmail.com>
2025-01-23 06:01:43 -0300

    Inicializando repositório

---
+++ src/main/java/med/voll/web_application/controller/LoginController.java
@@ -0,0 +1,12 @@
+package med.voll.web_application.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String carregaPaginaListagem(){
+        return "autenticacao/login";
+    }
+}
---
+++ src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
@@ -0,0 +1,45 @@
+package med.voll.web_application.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class ConfiguracoesSeguranca {
+
+    @Bean
+    public UserDetailsService dadosUsuariosCadastrados(){
+        UserDetails usuario1 = User.builder()
+                .username("joao@email.com")
+                .password("{noop}joao123")
+                .build();
+        UserDetails usuario2 = User.builder()
+                .username("maria@email.com")
+                .password("{noop}maria123")
+                .build();
+        return new InMemoryUserDetailsManager(usuario1, usuario2);
+    }
+
+    @Bean
+    public SecurityFilterChain filtrosSeguranca(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(req -> {
+                        req.requestMatchers("/css/**", "/js/**", "/assets/**").permitAll();
+                        req.anyRequest().authenticated();
+                    })
+                .formLogin(form -> form.loginPage("/login")
+                        .defaultSuccessUrl("/")
+                        .permitAll())
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/login?logout")
+                        .permitAll())
+                .build();
+    }
+}
