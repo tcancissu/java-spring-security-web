4878b419b22f322e5576982b7d752024dc46e6f6
Thiago Cancissú <thiago.cancissu@gmail.com>
2025-01-25 14:57:36 -0300

    Inclusão da cadastro de pacientes

--- src/main/java/med/voll/web_application/controller/ConsultaController.java
+++ src/main/java/med/voll/web_application/controller/ConsultaController.java
@@ -5,8 +5,10 @@ import med.voll.web_application.domain.RegraDeNegocioException;
 import med.voll.web_application.domain.consulta.ConsultaService;
 import med.voll.web_application.domain.consulta.DadosAgendamentoConsulta;
 import med.voll.web_application.domain.medico.Especialidade;
+import med.voll.web_application.domain.usuario.Usuario;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -43,7 +45,7 @@ public class ConsultaController {
         if (id != null) {
             model.addAttribute("dados", service.carregarPorId(id));
         } else {
-            model.addAttribute("dados", new DadosAgendamentoConsulta(null, null, "", null, null));
+            model.addAttribute("dados", new DadosAgendamentoConsulta(null, null, null, null, null));
         }
 
         return PAGINA_CADASTRO;
