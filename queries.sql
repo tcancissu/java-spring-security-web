ee6eb334fb9e5090a5a72ff208cdb1cd2fd2de27
tcancissu <thiago.cancissu@gmail.com>
2025-01-27 21:32:54 -0300

    Atualização dos scripts de consulta na BD

--- queries.sql
+++ queries.sql
@@ -1,10 +1,13 @@
 use vollmed_web;
 
-select * from medicos;
-select * from consultas;
 select * from usuarios;
+select * from medicos;
+select * from pacientes;
+select * from consultas;
 
 -- senha 123456 > $2y$10$CVJW98Uu69yvFa5KSpnhOuRyW6VzgHCWxUXIR/YpM1t6uA80sxVyu
-insert into usuarios(nome, email, senha) VALUES ('joao', 'joa0@email.com', '$2y$10$CVJW98Uu69yvFa5KSpnhOuRyW6VzgHCWxUXIR/YpM1t6uA80sxVyu'); 
-update usuarios set nome = 'João' where id=1; 
+insert into usuarios(nome, email, senha) VALUES ('joao', 'joao@email.com', '$2y$10$CVJW98Uu69yvFa5KSpnhOuRyW6VzgHCWxUXIR/YpM1t6uA80sxVyu'); 
+
+update usuarios set senha = '$2y$10$CVJW98Uu69yvFa5KSpnhOuRyW6VzgHCWxUXIR/YpM1t6uA80sxVyu' where id=3; 
+
 
